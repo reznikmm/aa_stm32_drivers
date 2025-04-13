c3be67df50ac880de0da4206cfa8d8818cf2da0b
Maxim Reznik <reznikmm@gmail.com>
2025-04-13 14:12:18 +0300

    Rename Drivers to STM32
    These drivers works for STM32 only, so let's name them this way.

--- demos/uart/uart.adb
+++ demos/uart/uart.adb
@@ -6,24 +6,24 @@
 with A0B.Callbacks;
 with Ada.Synchronous_Task_Control;
 with Devices;
-with Drivers.UART.USART_1;
+with STM32.UART.USART_1;
 with Suspension_Object_Callbacks;
 
 procedure UART is
-   USART_1 : Drivers.UART.USART_1.Device renames Devices.USART_1;
+   USART_1 : STM32.UART.USART_1.Device renames Devices.USART_1;
    Buffer  : String := "Hello, World!";
    Signal  : aliased Ada.Synchronous_Task_Control.Suspension_Object;
    Done    : constant A0B.Callbacks.Callback :=
      Suspension_Object_Callbacks.Create_Callback (Signal);
 begin
-   Drivers.UART.USART_1.Configure
+   STM32.UART.USART_1.Configure
      (USART_1,
       TX    => ('A', 9),
       RX    => ('A', 10),
       Speed => 115_200);
 
    loop
-      Drivers.UART.USART_1.Start_Writing
+      STM32.UART.USART_1.Start_Writing
         (USART_1, Buffer'Address, Buffer'Length, Done);
 
       Ada.Synchronous_Task_Control.Suspend_Until_True (Signal);
