5c576cfe7197e38fc1d7ec6084601298a1f44be9
Max Reznik <reznikmm@gmail.com>
2026-07-19 07:01:14 +0300

    Add basic timers with IRQ

---
+++ source/impl/stm32-timer-basic_implementation.adb
@@ -0,0 +1,52 @@
+--  SPDX-FileCopyrightText: 2026 Max Reznik <reznikmm@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+----------------------------------------------------------------
+
+package body STM32.Timer.Basic_Implementation is
+
+   ----------------------------
+   -- Set_Callback --
+   ----------------------------
+
+   procedure Set_Callback (On_Update : A0B.Callbacks.Callback) is
+   begin
+      Device.Set_Callback (On_Update);
+   end Set_Callback;
+
+   ------------
+   -- Device --
+   ------------
+
+   protected body Device is
+
+      ----------------------------
+      -- Set_Callback --
+      ----------------------------
+
+      procedure Set_Callback (On_Update : A0B.Callbacks.Callback)
+      is
+      begin
+         Callback := On_Update;
+         Periph.SR.UIF := False;
+         Periph.DIER.UIE := A0B.Callbacks.Is_Set (Callback);
+      end Set_Callback;
+
+      -----------------------
+      -- Interrupt_Handler --
+      -----------------------
+
+      procedure Interrupt_Handler is
+      begin
+         if Periph.SR.UIF then
+            Periph.SR.UIF := False;
+
+            if A0B.Callbacks.Is_Set (Callback) then
+               A0B.Callbacks.Emit (Callback);
+            end if;
+         end if;
+      end Interrupt_Handler;
+
+   end Device;
+
+end STM32.Timer.Basic_Implementation;
---
+++ source/impl/stm32-timer-tim_7.adb
@@ -0,0 +1,22 @@
+--  SPDX-FileCopyrightText: 2026 Max Reznik <reznikmm@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+----------------------------------------------------------------
+
+with STM32.Registers.RCC;
+
+package body STM32.Timer.TIM_7 is
+
+   procedure Reset is
+   begin
+      STM32.Registers.RCC.RCC_Periph.APB1ENR.TIM_EN_2_7 (7) := True;
+      STM32.Registers.RCC.RCC_Periph.APB1RSTR.TIM_EN_2_7 (7) := True;
+      STM32.Registers.RCC.RCC_Periph.APB1RSTR.TIM_EN_2_7 (7) := False;
+   end Reset;
+
+   procedure Set_Frequency (Value : Positive) is
+   begin
+      Implementation.Set_Frequency (Value, STM32.System_Clocks.TIMCLK1);
+   end Set_Frequency;
+
+end STM32.Timer.TIM_7;
